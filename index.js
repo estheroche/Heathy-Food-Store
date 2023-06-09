be12eba480f6d1e35698fa0d96765291dfe2126c
estheroche <estheraladioche569@gmail.com>
2023-06-09 11:31:43 -0500

    feat: scriptt

---
+++ index.js
@@ -0,0 +1,49 @@
+const images = document.querySelectorAll('.slider')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+let current = 0
+images.forEach((image, index) => {
+    image.style.left = `${index * 100}%`
+})
+
+let interval = settingInterval()
+
+ function settingInterval(){
+    setInterval(() => {
+        current += 1
+        slide()
+    }, 11000);
+ }
+
+
+function slide(){
+    if(current > images.length -1 ){
+        current = 0
+    }
+    if(current < 0){
+        current = 0
+    }
+    console.log("best")
+    images.forEach((slide) => {
+        slide.style.transform = `translateX(-${current * 100}%)`
+    })
+}
+
+
+next.addEventListener(('click'), () => {
+    clearInterval(interval)
+    interval = settingInterval()
+    if(current < images.length){
+        current = current + 1
+        slide()
+    }
+})
+
+prev.addEventListener(('click'), () => {
+    clearInterval(interval)
+    interval = settingInterval()
+    if(current > 0){
+        current = current - 1
+        slide()
+    }
+})
